4451f26b10ba45960304de47a2fd325f29263bc5
fatimanadinebh <1211107836@student.mmu.edu.my>
2025-06-21 01:33:35 +0800

    Update app.js
    gozilla scaling agaaaain

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ class App{
         const godzilla = gltf2.scene;
         godzilla.name = "Godzilla";
         godzilla.position.set(2, 0, 9);
-	godzilla.scale.set(60, 60, 60);
+	godzilla.scale.set(30, 30, 30);
         self.scene.add(godzilla);
         console.log("✅ Godzilla loaded", godzilla);
     },
