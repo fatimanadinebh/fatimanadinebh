6b38be79512bf6e5b659d7b8b100015dbad5dad2
fatimanadinebh <1211107836@student.mmu.edu.my>
2025-06-20 22:02:28 +0800

    Update app.js

--- app.js
+++ app.js
@@ -92,7 +92,6 @@ class App{
     }
     
 	loadCollege(){
-       
 		const loader = new GLTFLoader( ).setPath(this.assetsPath);
         const dracoLoader = new DRACOLoader();
         dracoLoader.setDecoderPath( './libs/three/js/draco/' );
